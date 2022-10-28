6f6e85dc38f0e030b5e29f1b39bd6d046ea33813
Caratapatti <108945117+Caratapatti@users.noreply.github.com>
2022-10-28 12:44:55 -0300

    28/10

--- estudo.js
+++ estudo.js
@@ -10,22 +10,22 @@ function total(){
     }
     if(!Number(j)){
       alert("O valor do juros deve ser um número.")
-      document.getElementById("Capital").value = "";
-      document.getElementById("Capital").focus();
+      document.getElementById("juros").value = "";
+      document.getElementById("juros").focus();
       return
   }
   if(!Number(t)){
-    alert("O valor do meses deve ser um número.")
-    document.getElementById("Capital").value = "";
-    document.getElementById("Capital").focus();
+    alert("A quantidade de meses deve ser um número.")
+    document.getElementById("meses").value = "";
+    document.getElementById("meses").focus();
     return
 }
     let m = 0;
     for(let i = 1; i <= t; i++){
         m = c * (1 + (j/100));
-        document.write("Valor domMês " + i + " valor: " + m +"<br>");
+        //document.write("Valor domMês " + i + " valor: " + m +"<br>");
         c = m;
     }
-    
-    document.write("Montante: " + m);
+    document.getElementById("total").innerHTML = m;
+    //document.write("Montante: " + m);
   }
